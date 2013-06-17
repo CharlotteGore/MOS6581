235f87cdbf145efcd3a20e717c0e2fb233c66760
Charlotte Gore <conspiracygore@gmail.com>
2013-06-17 23:41:12 +0100

    Added latest pictures, code and samples.

--- mos6581.cpp
+++ mos6581.cpp
@@ -2,53 +2,21 @@
 #include <SPI.h>
 #include "sid_registers.h"
 
-//#define SID_DEBUG
-
-
-void print_binary(int v, int num_places)
-{
-    int mask=0, n;
-
-    for (n=1; n<=num_places; n++)
-    {
-        mask = (mask << 1) | 0x0001;
-    }
-    v = v & mask;  // truncate v to specified number of places
-
-    while(num_places)
-    {
-
-        if (v & (0x0001 << num_places-1))
-        {
-             Serial.print("1");
-        }
-        else
-        {
-             Serial.print("0");
-        }
-
-        --num_places;
-        if(((num_places%4) == 0) && (num_places != 0))
-        {
-            Serial.print("_");
-        }
-    }
-}
 
 MOS6581::MOS6581(void){
-	ss = 7;
-	cs = 4;
-	clk = 9;
+	ss = 3;
+	cs = 2;
+	//clk = 9;
 	initialise();
-	startClock();
+	//startClock(); // Now using external crystal oscillator
 }
 
 MOS6581::MOS6581(byte shiftPin, byte sidPin, byte clockPin){
 	ss = shiftPin;
 	cs = sidPin;
-	clk = clockPin;
+	//                                                                                                                                                                                                             clk = clockPin;
 	initialise();
-	startClock();
+	//startClock(); // now using external crystal oscillator
 }
 
 void MOS6581::initialise(){
@@ -65,14 +33,9 @@ void MOS6581::initialise(){
 	// set the latch pins to their correct default states
 	digitalWrite(cs, HIGH);
 	digitalWrite(ss, LOW);
-
+	                                                                                                                                                                                                                                      
+	SPI.setClockDivider(SPI_CLOCK_DIV2);
 	SPI.begin();
-
-#ifdef SID_DEBUG
-	Serial.begin(57600);
-
-#endif
-
 }
 
 void MOS6581::reset(){
@@ -106,24 +69,13 @@ void MOS6581::transfer(byte address, byte value){
 	// turn on the shift register output
 	digitalWrite(ss, HIGH);
 	// wait a small amount of time for the shift register latch data in
-	delayMicroseconds(300);
+	delayMicroseconds(2);
 	// enable write mode on the SID
 	digitalWrite(cs, LOW);
-	// wait a small amount of time for the SID to pick up the data
-	delayMicroseconds(300);
+	// 2 microseconds should be enough for a single clock pulse to get through
+	delayMicroseconds(2);
 	// disable write mode on the SID
 	digitalWrite(cs, HIGH);
-
-#ifdef SID_DEBUG
-	print_binary(address, 5);
-	Serial.print("\t");
-	Serial.print(address, DEC);
-	Serial.print("\t");
-	print_binary(value, 8);
-	Serial.print("\t");
-	Serial.print(value, DEC);
-	Serial.println("");
-#endif
 
 }
 
@@ -142,107 +94,28 @@ void MOS6581::voicePulseWidth(byte lowAddress, byte highAddress, word frequency)
 	transfer(highAddress, high);
 }
 
-void MOS6581::voiceOneFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice one frequency");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voiceFrequency(SID_V1_FL, SID_V1_FH, frequency);
+void MOS6581::setFrequency(byte voice, word frequency){
+	if(voice == 0){
+		voiceFrequency(SID_V1_FL, SID_V1_FH, frequency);
+	}else if(voice == 1){
+		voiceFrequency(SID_V2_FL, SID_V2_FH, frequency);
+	}else if(voice == 2){
+		voiceFrequency(SID_V3_FL, SID_V3_FH, frequency);
+	}
 }
 
-void MOS6581::voiceTwoFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice two frequency");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voiceFrequency(SID_V2_FL, SID_V2_FH, frequency);
-}
-
-void MOS6581::voiceThreeFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice three frequency");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voiceFrequency(SID_V3_FL, SID_V3_FH, frequency);
-}
-
-void MOS6581::voiceOnePulseWidthFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice one pulse width");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voicePulseWidth(SID_V1_PWL, SID_V1_PWH, frequency);
-}
-
-void MOS6581::voiceTwoPulseWidthFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice two pulse width");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voicePulseWidth(SID_V2_PWL, SID_V2_PWH, frequency);
-}
-
-void MOS6581::voiceThreePulseWidthFrequency(word frequency){
-
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Voice three pulse width");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
-	voicePulseWidth(SID_V3_PWL, SID_V3_PWH, frequency);
+void MOS6581::setPulseWidth(byte voice, word frequency){
+	if(voice == 0){
+		voicePulseWidth(SID_V1_PWL, SID_V1_PWH, frequency);
+	}else if(voice == 1){
+		voicePulseWidth(SID_V2_PWL, SID_V2_PWH, frequency);
+	}else if(voice == 2){
+		voicePulseWidth(SID_V3_PWL, SID_V3_PWH, frequency);
+	}
 }
 
 void MOS6581::filterFrequency(word frequency){
 
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Filter frequency");
-	Serial.print("\t");
-	print_binary(frequency, 16);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
 	// shift the number along 5 bits to get the high frequency
 	byte low = lowByte(frequency) & B00000111;
 	byte high = highByte(frequency << 5);
@@ -253,16 +126,6 @@ void MOS6581::filterFrequency(word frequency){
 
 void MOS6581::filterResonance(byte frequency){
 
-#ifdef SID_DEBUG
-	Serial.println("");
-	Serial.print("Filter resonance");
-	Serial.print("\t");
-	print_binary(frequency, 8);
-	Serial.print("\t");
-	Serial.print(frequency, DEC);
-	Serial.println("");
-#endif
-
 	filter_register &= B00001111;
 
 	filter_register |= (frequency << 4);
@@ -271,156 +134,95 @@ void MOS6581::filterResonance(byte frequency){
 	// shift the number back 
 }
 
-void MOS6581::voiceOneFilterOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one filter on");
-#endif
-
-	filter_register |= SID_FILT_VOICE1;
+void MOS6581::setFilter(byte voice, boolean on){
+	if(voice == 0){
+		if(on){
+			filter_register |= SID_FILT_VOICE1;
+		}else{
+			filter_register &= ~SID_FILT_VOICE1;
+		}
+	}else if(voice == 1){
+		if(on){
+			filter_register |= SID_FILT_VOICE2;
+		}else{
+			filter_register &= ~SID_FILT_VOICE2;
+		}
+	}else if(voice == 2){
+		if(on){
+			filter_register |= SID_FILT_VOICE3;
+		}else{
+			filter_register &= ~SID_FILT_VOICE3;
+		}
+	}
 	transfer(SID_FL_RES_CT, filter_register);
 }
 
-void MOS6581::voiceTwoFilterOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two filter on");
-#endif
-
-	filter_register |= SID_FILT_VOICE2;
-	transfer(SID_FL_RES_CT, filter_register);
-}
-
-void MOS6581::voiceThreeFilterOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three filter on");
-#endif
-
-	filter_register |= SID_FILT_VOICE3;
-	transfer(SID_FL_RES_CT, filter_register);
-}
-
-void MOS6581::voiceOneFilterOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one filter off");
-#endif
-
-	filter_register &= ~SID_FILT_VOICE1;
-	transfer(SID_FL_RES_CT, filter_register);
-}
-
-void MOS6581::voiceTwoFilterOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two filter off");
-#endif
-
-	filter_register &= ~SID_FILT_VOICE2;
-	transfer(SID_FL_RES_CT, filter_register);
-}
-
-void MOS6581::voiceThreeFilterOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three filter off");
-#endif
-
-	filter_register &= ~SID_FILT_VOICE3;
-	transfer(SID_FL_RES_CT, filter_register);
-}
-
-void MOS6581::voiceOneMode(byte mode){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one mode");
-#endif
-
-	voice1_register &= B00000001;
-	voice1_register |= mode;
-	transfer(SID_V1_CT, voice1_register);
+void MOS6581::setVoice(byte voice, boolean on){
+	if(voice == 0){
+		if(on){
+			voice1_register |= B00000001;
+		}else{
+			voice1_register &= B11111110;
+		}
+		transfer(SID_V1_CT, voice1_register);
+	}else if(voice == 1){
+		if(on){
+			voice2_register |= B00000001;
+		}else{
+			voice2_register &= B11111110;
+		}
+		transfer(SID_V2_CT, voice2_register);
+	}else if(voice == 2){
+		if(on){
+			voice3_register |= B00000001;
+		}else{
+			voice3_register &= B11111110;
+		}
+		transfer(SID_V3_CT, voice3_register);
+	}
 
 }
 
-void MOS6581::voiceTwoMode(byte mode){
+void MOS6581::setMode(byte voice, byte mode){
 
-#ifdef SID_DEBUG
-	Serial.println("Voice two mode");
-#endif
-
-	voice2_register &= B00000001;
-	voice2_register |= mode;
-	transfer(SID_V2_CT, voice2_register);
-
+	if(voice == 0){
+		voice1_register &= B00000001;
+		voice1_register |= mode;
+		transfer(SID_V1_CT, voice1_register);
+	}else if(voice == 1){
+		voice2_register &= B00000001;
+		voice2_register |= mode;
+		transfer(SID_V2_CT, voice2_register);
+	}else if(voice == 2){
+		voice3_register &= B00000001;
+		voice3_register |= mode;
+		transfer(SID_V3_CT, voice3_register);
+	}else{
+	 
+	}
 }
 
-void MOS6581::voiceThreeMode(byte mode){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three mode");
-#endif
-
-	voice3_register &= B00000001;
-	voice3_register |= mode;
-	transfer(SID_V3_CT, voice3_register);
-
+void MOS6581::setADEnvelope(byte voice, byte attack, byte decay){
+	if(voice == 0){
+		transfer(SID_V1_AD,(decay & B00001111) | (attack << 4));
+	}else if(voice == 1){
+		transfer(SID_V2_AD,(decay & B00001111) | (attack << 4));
+	}else if(voice == 2){
+		transfer(SID_V3_AD,(decay & B00001111) | (attack << 4));
+	}
 }
 
-void MOS6581::voiceOneEnvelope(byte attack, byte decay, byte sustain, byte release){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one attack/decay");
-#endif
-
-	transfer(SID_V1_AD,(decay & B00001111) | (attack << 4));
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one sustain/release");
-#endif
-
-	transfer(SID_V1_SR,(release & B00001111) | (sustain << 4));
-
-}
-
-
-void MOS6581::voiceTwoEnvelope(byte attack, byte decay, byte sustain, byte release){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two attack/decay");
-#endif
-
-	transfer(SID_V2_AD,(decay & B00001111) | (attack << 4));
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two sustain/release");
-#endif
-
-	transfer(SID_V2_SR,(release & B00001111) | (sustain << 4));
-
-}
-
-void MOS6581::voiceThreeEnvelope(byte attack, byte decay, byte sustain, byte release){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three attack/decay");
-#endif
-
-	transfer(SID_V3_AD,(decay & B00001111) | (attack << 4));
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three sustain/release");
-#endif
-
-	transfer(SID_V3_SR,(release & B00001111) | (sustain << 4));
-
+void MOS6581::setSREnvelope(byte voice, byte sustain, byte release){
+	if(voice == 0){
+		transfer(SID_V1_SR,(release & B00001111) | (sustain << 4));
+	}else if(voice == 1){
+		transfer(SID_V2_SR,(release & B00001111) | (sustain << 4));
+	}else if(voice == 2){
+		transfer(SID_V3_SR,(release & B00001111) | (sustain << 4));
+	}
 }
 
 void MOS6581::volume(byte level){
-
-#ifdef SID_DEBUG
-	Serial.println("Set volume");
-#endif
 
 	mode_register &= B11110000;
 	mode_register |= (level & B00001111);
@@ -428,116 +230,10 @@ void MOS6581::volume(byte level){
 	transfer(SID_FL_MD_VL, mode_register);
 }
 
-void MOS6581::filterBP(){
-
-#ifdef SID_DEBUG
-	Serial.println("Setting filter band pass mode");
-#endif
+void MOS6581::setFilterMode(byte mode){
 
 	mode_register &= B00001111;
-	mode_register |= SID_FILT_BP;
+	mode_register |= mode;
 
 	transfer(SID_FL_MD_VL, mode_register);
-}
-
-void MOS6581::filterLP(){
-
-#ifdef SID_DEBUG
-	Serial.println("Setting filter low pass mode");
-#endif
-
-	mode_register &= B00001111;
-	mode_register |= SID_FILT_LP;
-
-	transfer(SID_FL_MD_VL, mode_register);
-}
-
-void MOS6581::filterHP(){
-
-#ifdef SID_DEBUG
-	Serial.println("Setting filter high pass mode");
-#endif
-
-	mode_register &= B00001111;
-	mode_register |= SID_FILT_HP;
-
-	transfer(SID_FL_MD_VL, mode_register);
-}
-
-void MOS6581::filterNotch(){
-
-#ifdef SID_DEBUG
-	Serial.println("Setting filter notch mode");
-#endif
-
-	mode_register &= B00001111;
-	mode_register |= (SID_FILT_HP | SID_FILT_LP);
-
-	transfer(SID_FL_MD_VL, mode_register);
-}
-
-void MOS6581::voiceOneOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one on");
-#endif
-
-	voice1_register |= B00000001;
-	transfer(SID_V1_CT, voice1_register);
-
-}
-
-void MOS6581::voiceTwoOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two on");
-#endif
-
-	voice2_register |= B00000001;
-	transfer(SID_V2_CT, voice2_register);
-
-}
-
-void MOS6581::voiceThreeOn(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three on");
-#endif
-
-	voice3_register |= B00000001;
-	transfer(SID_V3_CT, voice3_register);
-
-}
-
-void MOS6581::voiceOneOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice one off");
-#endif
-
-	voice1_register &= B11111110;
-	transfer(SID_V1_CT, voice1_register);
-
-}
-
-void MOS6581::voiceTwoOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice two off");
-#endif
-
-	voice2_register &= B11111110;
-	transfer(SID_V2_CT, voice2_register);
-
-}
-
-void MOS6581::voiceThreeOff(){
-
-#ifdef SID_DEBUG
-	Serial.println("Voice three off");
-#endif
-
-	voice3_register &= B11111110;
-	transfer(SID_V3_CT, voice3_register);
-
 }
--- mos6581.h
+++ mos6581.h
@@ -10,15 +10,17 @@
 #define SID_NOISE 		128
 #define SID_SQUARE		64
 #define SID_RAMP		32
-#define SID_TRIANGLE		16
+#define SID_TRIANGLE	16
 #define SID_TEST		8
-#define SID_RING		4
-#define SID_SYNC		2
+#define SID_RING		20
+#define SID_SYNC		66
+#define SID_OFF			0
 
 #define SID_3OFF		128
 #define SID_FILT_HP		64
 #define SID_FILT_BP		32
 #define SID_FILT_LP		16
+#define SID_FILT_OFF	0
 
 #define SID_FILT_VOICE1	1
 #define SID_FILT_VOICE2 2
@@ -47,53 +49,29 @@ public:
 	MOS6581(byte shiftPin, byte sidPin, byte clockPin);
 	void reset(void);
 	void transfer(byte address, byte value);
-	void voiceOneOn();
-	void voiceTwoOn();
-	void voiceThreeOn();
 
-	void voiceOneOff();
-	void voiceTwoOff();
-	void voiceThreeOff();
+	void setVoice(byte voice, boolean on);
+	void setFilter(byte voice, boolean on);
 
-	void voiceOneFilterOn();
-	void voiceTwoFilterOn();
-	void voiceThreeFilterOn();
-
-	void voiceOneFilterOff();
-	void voiceTwoFilterOff();
-	void voiceThreeFilterOff();
-
-	// fundamental frequency of waveform generator. 16bit number.
-	void voiceOneFrequency(word frequency);
-	void voiceTwoFrequency(word frequency);
-	void voiceThreeFrequency(word frequency);
+	// fundamental frequency of waveform generator. 16bit number
+	void setFrequency(byte voice, word frequency);
 
 	// duty cycle of square waves. 12bit number
-	void voiceOnePulseWidthFrequency(word frequency);
-	void voiceTwoPulseWidthFrequency(word frequency);
-	void voiceThreePulseWidthFrequency(word frequency);
+	void setPulseWidth(byte voice, word frequency);
 
-	void voiceOneMode(byte mode);
-	void voiceTwoMode(byte mode);
-	void voiceThreeMode(byte mode);
+	void setMode(byte voice, byte mode);
 
-	void voiceOneEnvelope(byte attack, byte decay, byte sustain, byte release);
-	void voiceTwoEnvelope(byte attack, byte decay, byte sustain, byte release);
-	void voiceThreeEnvelope(byte attack, byte decay, byte sustain, byte release);
+	void setADEnvelope(byte voice, byte attack, byte decay);
+	void setSREnvelope(byte voice, byte sustain, byte release);
 
 	// filter volume and output
 	void volume(byte value);
-	void filterOff();
-	void filterLP();
-	void filterHP();
-	void filterBP();
-	void filterNotch();
+
+	void setFilterMode(byte mode);
 
 	// set the filter frequency. 11bit number
 	void filterFrequency(word frequency);
 	void filterResonance(byte resonance);
-
-
 
 };
 
